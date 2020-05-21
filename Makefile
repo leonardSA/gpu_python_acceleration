890b858d4e9d675d228d996d66fdf1041d12beb3
leonardSA <leonard.stephenauguste@gmail.com>
2020-05-21 12:28:17 +0200

    scripts,configure_python_env: add argument for pip version

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 SCRIPTS_DIR=scripts
 PY_REQ=.py_requirements.txt
+PIP_VER=pip3
 PY_VER=python3.8
 PY_ENV_DIR=.environment
 
 environment-setup:
-	./$(SCRIPTS_DIR)/configure_python_env.sh $(PY_ENV_DIR) $(PY_VER) $(PY_REQ)
-
-
+	./$(SCRIPTS_DIR)/configure_python_env.sh $(PY_VER) $(PIP_VER) $(PY_ENV_DIR) $(PY_REQ)
--- scripts/configure_python_env.sh
+++ scripts/configure_python_env.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
     echo -e "\033[1;31mERROR: $0 arguments\033[0m"
     echo -e "\033[1;31mExpected: $0 ENVIORNMENT_DIR"\
             "PYTHON_VERSION REQUIREMENTS_FILE\033[0m"
     exit 1
 fi
 
-VENV_DIR=$1
-PY_VERSION=$2
-REQUIREMENTS=$3
+PY_VERSION=$1
+PIP_VERSION=$2
+VENV_DIR=$3
+REQUIREMENTS=$4
 
 if [ ! -d $VENV_DIR ]; then 
     $PY_VERSION -m venv $VENV_DIR ||\
@@ -21,7 +22,7 @@ fi
 
 if [ -f $REQUIREMENTS ]; then
     source $VENV_DIR/bin/activate
-    pip install -r $REQUIREMENTS 
+    $PIP_VERSION install -r $REQUIREMENTS 
 else
     echo -e "\033[1;33mWARNING: could not find the"\
         "requirements file $REQUIREMENTS\033[0m"
